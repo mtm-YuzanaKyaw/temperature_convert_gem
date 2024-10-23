dda3ba197fc5b233120aa2e65b1634095d85ee50
Yuzana Kyaw <mtm.yuzanakyaw@gmail.com>
2024-10-23 10:01:52 +0630

    update detail temperature

--- lib/temperature_convert_gem.rb
+++ lib/temperature_convert_gem.rb
@@ -5,7 +5,19 @@ require_relative "temperature_convert_gem/version"
 module TemperatureConvertGem
   class Error < StandardError; end
 
-  def self.convert_temperature(input)
+  def self.degreeFtoC(input)
+    puts "#{(input.to_f-32)*5/9}"
+  end
+
+  def self.degreeCtoF(input)
     puts "#{input.to_f * 9 / 5 + 32}"
   end
+
+  def self.degreeCtoKelvin(input)
+    puts "#{input.to_f+273.15}"
+  end
+
+  # def self.convert_temperature(input)
+  #   puts "#{input.to_f * 9 / 5 + 32}"
+  # end
 end
